38425ef83c8cb14f46c3060ad7182a4f4b8e04b6
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-04-21 09:57:09 +0900

    fix(main.ts): fix variable name

--- srcs/main.ts
+++ srcs/main.ts
@@ -1,6 +1,11 @@
 const SPREADSHEET_ID = '' // URL:https://docs.google.com/spreadsheets/d/***/edit#gid=0 (*** is the ID)
 const BKUP_FOLDER_ID = '' // URL:https://drive.google.com/drive/folders/*** (*** is the ID)
 
+function main() {
+	backupFile();
+	removeOldFile();
+}
+
 function backupFile() {
 	let srcFile: File = DriveApp.getFileById(SPREADSHEET_ID);
 	let bkupFolder: Folder = DriveApp.getFolderById(BKUP_FOLDER_ID);
@@ -9,17 +14,12 @@ function backupFile() {
 
 function removeOldFile() {
 	let leastValidDate: Date = new Date();
-	leastValidDate.setDate(validDate.getDate() - 30);
+	leastValidDate.setDate(leastValidDate.getDate() - 30);
 	let bkupFolder: Folder = DriveApp.getFolderById(BKUP_FOLDER_ID);
-	let files: FileIterator = bkup_folder.getFiles();
+	let files: FileIterator = bkupFolder.getFiles();
 	while (files.hasNext()) {
 		let file: File = files.next();
 		if (file.getDateCreated() <= leastValidDate)
 			file.setTrashed(true);
 	}
-}
-
-function main() {
-	backupFile();
-	removeOldFile();
 }
