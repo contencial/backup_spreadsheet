0ba90abf371f8f081cb7403b19fd2bb1de342dbe
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-04-21 10:44:09 +0900

    fix(removeoldfile.ts): add parameter: retention_period
    - Now it is able to define retention_period at main.ts
    - More convinient to modify retention period

--- srcs/main.ts
+++ srcs/main.ts
@@ -1,7 +1,8 @@
 function main() {
-	const SPREADSHEET_ID = '' // URL:https://docs.google.com/spreadsheets/d/***/edit#gid=0 (*** is the ID)
-	const BKUP_FOLDER_ID = '' // URL:https://drive.google.com/drive/folders/*** (*** is the ID)
+	const SPREADSHEET_ID: string = ''; // URL:https://docs.google.com/spreadsheets/d/***/edit#gid=0 (*** is the ID)
+	const BKUP_FOLDER_ID: string = ''; // URL:https://drive.google.com/drive/folders/*** (*** is the ID)
+	const RETENTION_PERIOD: number = 30;
 
 	backupFile(SPREADSHEET_ID, BKUP_FOLDER_ID);
-	removeOldFile(BKUP_FOLDER_ID);
+	removeOldFile(BKUP_FOLDER_ID, RETENTION_PERIOD);
 }
--- srcs/removeOldFile.ts
+++ srcs/removeOldFile.ts
@@ -1,6 +1,6 @@
-function removeOldFile(folder_id: string) {
+function removeOldFile(folder_id: string, retention_period: number) {
 	let leastValidDate: Date = new Date();
-	leastValidDate.setDate(leastValidDate.getDate() - 30);
+	leastValidDate.setDate(leastValidDate.getDate() - retention_period);
 	let bkupFolder: Folder = DriveApp.getFolderById(folder_id);
 	let files: FileIterator = bkupFolder.getFiles();
 	while (files.hasNext()) {
