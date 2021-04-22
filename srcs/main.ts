7be8c6a4c339f5110a225c961a177661cde36cec
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-04-22 13:00:52 +0900

    fix(main.ts): set each ID to the property and use it
    - IDをベタ書きするのではなく、プロパティに設定して使用する形に変更

--- srcs/main.ts
+++ srcs/main.ts
@@ -1,6 +1,6 @@
 function main() {
-	const SPREADSHEET_ID: string = ''; // URL:https://docs.google.com/spreadsheets/d/***/edit#gid=0 (*** is the ID)
-	const BKUP_FOLDER_ID: string = ''; // URL:https://drive.google.com/drive/folders/*** (*** is the ID)
+	const SPREADSHEET_ID: string = PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID');
+	const BKUP_FOLDER_ID: string = PropertiesService.getScriptProperties().getProperty('BKUP_FOLDER_ID');
 	const RETENTION_PERIOD: number = 30;
 
 	backupFile(SPREADSHEET_ID, BKUP_FOLDER_ID);
